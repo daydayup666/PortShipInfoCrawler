355f58b5cacfc5b193fe1c9a693433ab47b74edc
jutao <836535860@qq.com>
2016-06-28 23:52:42 +0800

    完成提取船舶主要资料和基本资料

--- src/com/xmu/logic/ShipSpecificInfoParser.java
+++ src/com/xmu/logic/ShipSpecificInfoParser.java
@@ -1,8 +1,9 @@
 package com.xmu.logic;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.htmlparser.Node;
 import org.htmlparser.NodeFilter;
 import org.htmlparser.Parser;
 import org.htmlparser.filters.NodeClassFilter;
@@ -16,79 +17,118 @@ import org.htmlparser.util.ParserException;
 import com.xmu.javaBean.ShipBaseInfo;
 import com.xmu.javaBean.ShipMainInfo;
 
-
-
 public class ShipSpecificInfoParser {
 	ArrayList<String> shipInfoLinkList;
 	ArrayList<ShipMainInfo> shipMainInfosList = new ArrayList<>();
 	ArrayList<ShipBaseInfo> shipBaseInfosList = new ArrayList<>();
 	Parser parser;
-	public void shipInfoParser() {		
+
+	public void shipInfoParser() {
 		shipInfoLinkList = ShipDynamicInfoParser.shipInfoLinkList;
 		System.out.println(shipInfoLinkList);
-		for(String link : shipInfoLinkList) {
+		for (String link : shipInfoLinkList) {
 			try {
 				System.out.println("开始提取船舶具体信息...");
 				parser = new Parser(link);
-				
+
 				NodeFilter divFilter = new NodeClassFilter(Div.class);
 				NodeList divNodeList = parser.extractAllNodesThatMatch(divFilter);
-				//船舶主要信息
+				// 船舶主要信息
 				ShipMainInfo mainInfo = new ShipMainInfo();
-				//船舶基本信息
+				// 船舶基本信息
 				ShipBaseInfo baseInfo = new ShipBaseInfo();
-				
-				for(int i=0;i<divNodeList.size();i++) {
-					//具有Div标签的结点
-					Div divNode = (Div)divNodeList.elementAt(i);
-					//处理对class=table-div的结点
-					if(divNode.getAttribute("class")!=null&&
-							divNode.getAttribute("class").equals("table-div")) {
+			    
+				for (int i = 0; i < divNodeList.size(); i++) {
+					// 具有Div标签的结点
+					Div divNode = (Div) divNodeList.elementAt(i);
+					// 处理对class=table-div的结点
+					if (divNode.getAttribute("class") != null && divNode.getAttribute("class").equals("table-div")) {
 						NodeFilter bulletFilter = new NodeClassFilter(Bullet.class);
 						NodeList bulletList = divNode.getChildren().extractAllNodesThatMatch(bulletFilter, true);
-						
-						for(int j=0;j<bulletList.size();j++) {
-							Bullet bulletTag = (Bullet)bulletList.elementAt(j);
+						// 匹配船名
+						Pattern pattern = Pattern.compile("[a-zA-Z]+[\\w\\s]*[\\w]$");
+
+						for (int j = 0; j < bulletList.size(); j++) {
+							Bullet bulletTag = (Bullet) bulletList.elementAt(j);
 							NodeFilter spanFilter = new TagNameFilter("span");
-							NodeList spanList = bulletTag.getChildren().extractAllNodesThatMatch(spanFilter,true);
-							if(spanList.size()==2) {
-								if(spanList.elementAt(0).toPlainTextString().equals("船名：")){
-									System.out.println(spanList.elementAt(1).toPlainTextString());
-								}else if(spanList.elementAt(0).toPlainTextString().equals("中文船名：")){
-									System.out.println(spanList.elementAt(1).toPlainTextString());
-								}else if(spanList.elementAt(0).toPlainTextString().equals("呼号：")){
-									System.out.println(spanList.elementAt(1).toPlainTextString());
-								}else if(spanList.elementAt(0).toPlainTextString().equals("国籍：")){
-									System.out.println(spanList.elementAt(1).toPlainTextString());
-								}else if(spanList.elementAt(0).toPlainTextString().equals("船东：")){
-									System.out.println(spanList.elementAt(1).toPlainTextString());
-								}else if(spanList.elementAt(0).toPlainTextString().equals("船舶类型：")){
-									System.out.println(spanList.elementAt(1).toPlainTextString());
-								}else if(spanList.elementAt(0).toPlainTextString().equals("船长：")){
-									System.out.println(spanList.elementAt(1).toPlainTextString());
-								}else if(spanList.elementAt(0).toPlainTextString().equals("船宽：")){
-									System.out.println(spanList.elementAt(1).toPlainTextString());
-								}else if(spanList.elementAt(0).toPlainTextString().equals("吃水：")){
-									System.out.println(spanList.elementAt(1).toPlainTextString());
-								}else if(spanList.elementAt(0).toPlainTextString().equals("MMSI：")){
-									System.out.println(spanList.elementAt(1).toPlainTextString());
-								}else if(spanList.elementAt(0).toPlainTextString().equals("最后时间：")){
-									System.out.println(spanList.elementAt(1).toPlainTextString());
-								}else if(spanList.elementAt(0).toPlainTextString().equals("IMO：")){
-									System.out.println(spanList.elementAt(1).toPlainTextString());
+							NodeList spanList = bulletTag.getChildren().extractAllNodesThatMatch(spanFilter, true);
+							//提取船舶主要信息和基本信息保存到ShipMainInfo和ShipBaseInfo对象中
+							if (spanList.size() == 2) {
+								if (spanList.elementAt(0).toPlainTextString().equals("船名：")) {
+									Matcher matcher = pattern.matcher(spanList.elementAt(1).toPlainTextString());
+									while (matcher.find()) {
+										mainInfo.setShipName(matcher.group());
+										baseInfo.setShipName(matcher.group());
+										System.out.println("船名：" + matcher.group());
+									}
+									// System.out.println(spanList.elementAt(1).toPlainTextString());
+								} else if (spanList.elementAt(0).toPlainTextString().equals("中文船名：")) {
+									mainInfo.setShipChineseName(spanList.elementAt(1).toPlainTextString());
+									baseInfo.setShipChineseName(spanList.elementAt(1).toPlainTextString());
+									System.out.println("中文船名：" + spanList.elementAt(1).toPlainTextString());
+								} else if (spanList.elementAt(0).toPlainTextString().equals("呼号：")) {
+									mainInfo.setCallLetter(spanList.elementAt(1).toPlainTextString());
+									System.out.println("呼号：" + spanList.elementAt(1).toPlainTextString());
+								} else if (spanList.elementAt(0).toPlainTextString().equals("国籍：")) {
+									mainInfo.setNationality(spanList.elementAt(1).toPlainTextString());
+									System.out.println("国籍：" + spanList.elementAt(1).toPlainTextString());
+								} else if (spanList.elementAt(0).toPlainTextString().equals("船东：")) {
+									mainInfo.setShipOwner(spanList.elementAt(1).toPlainTextString());
+									System.out.println("船东：" + spanList.elementAt(1).toPlainTextString());
+								} else if (spanList.elementAt(0).toPlainTextString().equals("船舶类型：")) {
+									mainInfo.setShipType(spanList.elementAt(1).toPlainTextString());
+									System.out.println("船舶类型：" + spanList.elementAt(1).toPlainTextString());
+								} else if (spanList.elementAt(0).toPlainTextString().equals("船长：")) {
+									double length = Double.valueOf(spanList.elementAt(1).toPlainTextString());
+									mainInfo.setShipLength(length);
+									System.out.println("船长：" + spanList.elementAt(1).toPlainTextString());
+								} else if (spanList.elementAt(0).toPlainTextString().equals("船宽：")) {
+									double width = Double.valueOf(spanList.elementAt(1).toPlainTextString());
+									mainInfo.setShipWidth(width);
+									System.out.println("船宽：" + spanList.elementAt(1).toPlainTextString());
+								} else if (spanList.elementAt(0).toPlainTextString().equals("吃水：")) {
+									Pattern patt = Pattern.compile("\\d+\\.?\\d*");
+									Matcher mat = patt.matcher(spanList.elementAt(1).toPlainTextString());
+									while(mat.find()) {
+										double draft = Double.valueOf(mat.group());
+										mainInfo.setDraft(draft);
+										System.out.println("吃水：" + mat.group());
+									}		
+								} else if (spanList.elementAt(0).toPlainTextString().equals("MMSI：")) {
+									mainInfo.setMMSI(Integer.valueOf(spanList.elementAt(1).toPlainTextString()));
+									baseInfo.setMMSI(Integer.valueOf(spanList.elementAt(1).toPlainTextString()));
+									System.out.println("MMSI：" + spanList.elementAt(1).toPlainTextString());
+								} else if (spanList.elementAt(0).toPlainTextString().equals("最后时间：")) {
+									mainInfo.setLastTime(spanList.elementAt(1).toPlainTextString());
+									System.out.println("最后时间：" + spanList.elementAt(1).toPlainTextString());
+								} else if (spanList.elementAt(0).toPlainTextString().equals("IMO：")) {
+									mainInfo.setIMO(Integer.valueOf(spanList.elementAt(1).toPlainTextString()));
+									baseInfo.setIMO(Integer.valueOf(spanList.elementAt(1).toPlainTextString()));
+									System.out.println("IMO:" + spanList.elementAt(1).toPlainTextString());
+								}else if (spanList.elementAt(0).toPlainTextString().equals("原船名：")) {
+									baseInfo.setOriginShipName(spanList.elementAt(1).toPlainTextString());
+									System.out.println("原船名:" + spanList.elementAt(1).toPlainTextString());
+								}else if (spanList.elementAt(0).toPlainTextString().equals("原国籍：")) {
+									baseInfo.setOriginNationality(spanList.elementAt(1).toPlainTextString());
+									System.out.println("原国籍:" + spanList.elementAt(1).toPlainTextString());
+								}else if (spanList.elementAt(0).toPlainTextString().equals("造船厂：")) {
+									baseInfo.setCreateShipFactory(spanList.elementAt(1).toPlainTextString());
+									System.out.println("造船厂:" + spanList.elementAt(1).toPlainTextString());
+								}else if (spanList.elementAt(0).toPlainTextString().equals("造船地点：")) {
+									baseInfo.setCreateShipPlace(spanList.elementAt(1).toPlainTextString());
+									System.out.println("造船地点:" + spanList.elementAt(1).toPlainTextString());
+								}else if (spanList.elementAt(0).toPlainTextString().equals("造船时间：")) {
+									baseInfo.setCreateShipTime(spanList.elementAt(1).toPlainTextString());
+									System.out.println("造船时间:" + spanList.elementAt(1).toPlainTextString());
 								}
-									
-								
 							}
 						}
-						
 					}
 				}
 			} catch (ParserException e) {
-			
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 }
